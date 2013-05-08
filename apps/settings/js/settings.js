eb4817319da9e5219959203738fb23112e05b641
crh0716 <crh0716@gmail.com>
2013-05-08 13:06:54 +0800

    gaia-local#16 - Change to two columns

--- apps/settings/js/settings.js
+++ apps/settings/js/settings.js
@@ -692,58 +692,19 @@ window.addEventListener('load', function loadSettings() {
     // load panel (+ dependencies) if necessary -- this should be synchronous
     lazyLoad(newPanel);
 
-    // switch previous/current/forward classes
-    // FIXME: The '.peek' is here to avoid an ugly white
-    // flickering when transitioning (gecko 18)
-    // the forward class helps us 'peek' in the right direction
-    oldPanel.className = newPanel.className ? 'peek' : 'peek previous forward';
-    newPanel.className = newPanel.className ?
-                           'current peek' : 'peek current forward';
-    oldHash = hash;
-
-    /**
-     * Most browsers now scroll content into view taking CSS transforms into
-     * account.  That's not what we want when moving between <section>s,
-     * because the being-moved-to section is offscreen when we navigate to its
-     * #hash.  The transitions assume the viewport is always at document 0,0.
-     * So add a hack here to make that assumption true again.
-     * https://bugzilla.mozilla.org/show_bug.cgi?id=803170
-     */
-    if ((window.scrollX !== 0) || (window.scrollY !== 0)) {
-      window.scrollTo(0, 0);
+    oldPanel.className = 'previous';
+    newPanel.className = 'current';
+    switch (newPanel.id) {
+      case 'about-licensing':
+        // Workaround for bug 825622, remove when fixed
+        var iframe = document.getElementById('os-license');
+        iframe.src = iframe.dataset.src;
+        break;
+      case 'wifi':
+        PerformanceTestingHelper.dispatch('settings-panel-wifi-visible');
+        break;
     }
-
-    window.addEventListener('transitionend', function paintWait() {
-      window.removeEventListener('transitionend', paintWait);
-
-      // We need to wait for the next tick otherwise gecko gets confused
-      setTimeout(function nextTick() {
-        oldPanel.classList.remove('peek');
-        oldPanel.classList.remove('forward');
-        newPanel.classList.remove('peek');
-        newPanel.classList.remove('forward');
-
-        // Bug 818056 - When multiple visible panels are present,
-        // they are not painted correctly. This appears to fix the issue.
-        // Only do this after the first load
-        if (oldPanel.className === 'current')
-          return;
-
-        oldPanel.addEventListener('transitionend', function onTransitionEnd() {
-          oldPanel.removeEventListener('transitionend', onTransitionEnd);
-          switch (newPanel.id) {
-            case 'about-licensing':
-              // Workaround for bug 825622, remove when fixed
-              var iframe = document.getElementById('os-license');
-              iframe.src = iframe.dataset.src;
-              break;
-            case 'wifi':
-              PerformanceTestingHelper.dispatch('settings-panel-wifi-visible');
-              break;
-          }
-        });
-      });
-    });
+    oldHash = hash;
   }
 
   function handleRadioAndCardState() {
