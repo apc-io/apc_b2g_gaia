7c21a7b132b0d2b5d2752f4244b15d8aa34f0399
Tung <buison.tung@nomovok.com>
2013-11-06 14:05:32 +0700

    Detect hw keyboard

--- apps/system/js/keyboard_manager.js
+++ apps/system/js/keyboard_manager.js
@@ -91,6 +91,7 @@ var KeyboardManager = {
   },
   keyboardHeight: 0,
   isOutOfProcessEnabled: false,
+  _onFocus: false,
 
   init: function km_init() {
     // generate typeTable
@@ -123,6 +124,24 @@ var KeyboardManager = {
 
     this.keyboardFrameContainer.classList.add('hide');
 
+    var hwkeyboardManager = window.navigator.hardwareKeyboardManager;
+    hwkeyboardManager.addEventListener('hwkeyboardpresentchange', function(e) {
+      var self = this;
+      if (hwkeyboardManager.isPresent && self.keyboardHeight) {
+        self._debug("hardware keyboard plugged, hide virtual keyboard");
+        self.hideKeyboard();
+      } else if (!hwkeyboardManager.isPresent && self._onFocus) {
+        self._debug("hardware keyboard unplugged, show virtual keyboard");
+        // if we already have layouts for the group, no need to check default
+        self.setKeyboardToShow(this.showingLayout.type);
+        self.showKeyboard();
+
+        // We also want to show the permanent notification
+        // in the UtilityTray.
+        self.showIMESwitcher();
+      }
+    }.bind(this));
+
     // get enabled keyboard from mozSettings, parse their manifest
 
     // For Bug 812115: hide the keyboard when the app is closed here,
@@ -137,6 +156,8 @@ var KeyboardManager = {
       var type = evt.detail.type;
       switch (type) {
         case 'inputmethod-showall':
+          if (navigator.hardwareKeyboardManager.isPresent)
+            break;
           this.showAll();
           break;
         case 'inputmethod-next':
@@ -278,6 +299,7 @@ var KeyboardManager = {
 
       if (type === 'blur') {
         self._debug('get blur event');
+        self._onFocus = false;
         self.hideKeyboard();
         self.hideIMESwitcher();
       } else {
@@ -288,7 +310,11 @@ var KeyboardManager = {
           // ensure the helper has apps and settings data first:
           KeyboardHelper.getLayouts(showKeyboard);
         } else {
-          showKeyboard();
+          self._onFocus = true;
+          if (!navigator.hardwareKeyboardManager.isPresent) {
+            self._debug("====hw keyboard not present, show vkb");
+            showKeyboard();
+          }
         }
 
 
