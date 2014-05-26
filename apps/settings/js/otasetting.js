432451073739bf7fedd1244f5f78b4aa4d70ae27
TienManhPtit <ninhmanh.tien@nomovok.vn>
2014-05-26 16:36:19 +0700

    Change OTA setting

--- apps/settings/js/otasetting.js
+++ apps/settings/js/otasetting.js
@@ -1,6 +1,35 @@
 /**
- * Created by osbuilder on 5/13/14.
+ * Created by Manh Tien on 5/13/14.
  */
 
 
 'use strict';
+
+
+var network = window.navigator.onLine;
+
+
+
+function checkfotNetwork(){
+
+    if(navigator.onLine){
+        window.parent.alert("You're online!!");
+    }
+    else
+    {
+        window.parent.alert("You need connect Internet!!");
+    }
+}
+
+var clickHandler = {
+    'check-for-update': function () {
+        checkfotNetwork();
+    }
+
+    window.addEventListener("offline", function(e) {do something})
+}
+
+document.body.addEventListener('click',function(evt){
+    if(clickHandler[evt.target.id])
+        clickHandler[evt.target.id || evt.target.dataset.fb].call(this,evt);
+});
