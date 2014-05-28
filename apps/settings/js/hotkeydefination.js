a715736a6c372d3088d005ce0127e83da56c660c
TienManhPtit <ninhmanh.tien@nomovok.vn>
2014-05-28 17:12:32 +0700

    Change UX hot key Definition

--- apps/settings/js/hotkeydefination.js
+++ apps/settings/js/hotkeydefination.js
@@ -1,3 +1,45 @@
 /**
  * Created by TienNM on 5/16/14.
  */
+
+var metaChar = false;
+var exampleKey = 16;
+
+function keyEvent(event) {
+    var key = event.keyCode || event.which;
+    var keychar = String.fromCharCode(key);
+
+    if (key != exampleKey) {
+        if (metaChar) {
+            window.parent.alert("Combination of metaKey + " + keychar);
+            metaChar = false;
+        } else {
+            window.parent.alert("You Changed Key : " + keychar);
+        }
+    }
+
+    if(document.getElementById("key-home-check").checked){
+        document.getElementById("key-home").innerHTML = keychar;
+    }else
+    if(document.getElementById("key-back-check").checked){
+        document.getElementById("key-back").innerHTML = keychar;
+    }else
+    if(document.getElementById("key-menu-check").checked){
+        document.getElementById("key-menu").innerHTML = keychar;
+    }else
+    if(document.getElementById("key-volume-down-check").checked){
+        document.getElementById("key-volume-up").innerHTML = keychar;
+    }else
+    if(document.getElementById("key-volume-up-check").checked){
+        document.getElementById("key-volume-down").innerHTML = keychar;
+    }else
+    if(document.getElementById("key-down-check").checked){
+        document.getElementById("key-down").innerHTML = keychar;
+    }else
+    if(document.getElementById("key-up-check").checked){
+        document.getElementById("key-up").innerHTML = keychar;
+    }
+
+}
+
+document.getElementById("hotkey-setup").addEventListener("keydown",keyEvent,false);
