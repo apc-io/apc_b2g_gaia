3f17733cf76b2ea23d084c385650dca5a378aee4
TienManhPtit <ninhmanh.tien@nomovok.vn>
2014-05-29 15:53:23 +0700

    Change UX OTA Setting

--- apps/settings/js/otasetting.js
+++ apps/settings/js/otasetting.js
@@ -4,32 +4,62 @@
 
 
 'use strict';
-
-
-var network = window.navigator.onLine;
-
-
+var downloadDialog = null;
 
 function checkfotNetwork(){
 
-    if(navigator.onLine){
-        window.parent.alert("You're online!!");
+    var online = (navigator && 'onLine' in navigator) ? navigator.onLine : true;
+//    if(online === true){
+//        window.parent.alert("You're online!!");
+//    }
+//    else
+//    {
+//        window.parent.alert("You need connect Internet!!");
+//    }
+
+    downloadDialog = document.getElementById('updates-download-dialog');
+    downloadDialog.style.display = "block";
+
+}
+
+function configUpdate(){
+    var configForm = document.getElementById("config-update-form");
+    if(configForm.style.display == "none"){
+        configForm.style.display = "block"
     }
     else
-    {
-        window.parent.alert("You need connect Internet!!");
-    }
+        configForm.style.display = "none";
 }
+
+//function changeTextURL(){
+//    if(document.getElementById("update-url").textContent.length > 0){
+//        downloadDialog = document.getElementById('updates-download-dialog').disabled = false;
+//    }
+//    else
+//    {
+//        downloadDialog = document.getElementById('updates-download-dialog').disabled = true;
+//    }
+//}
 
 var clickHandler = {
     'check-for-update': function () {
         checkfotNetwork();
+    },
+    'updates-later-button': function () {
+        downloadDialog = document.getElementById('updates-download-dialog');
+        downloadDialog.style.display = "none";
+    },
+    'updates-download-button': function () {
+        checkfotNetwork();
+    },
+    'config-for-update': function () {
+        configUpdate();
     }
-
-    window.addEventListener("offline", function(e) {do something})
 }
 
 document.body.addEventListener('click',function(evt){
     if(clickHandler[evt.target.id])
         clickHandler[evt.target.id || evt.target.dataset.fb].call(this,evt);
 });
+
+//document.getElementById("update-url").addEventListener("change",changeTextURL,false);
