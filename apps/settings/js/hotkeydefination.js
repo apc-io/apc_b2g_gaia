13d5b6986f9c365171e6a02d53f76a55c2faff98
TienManhPtit <ninhmanh.tien@nomovok.vn>
2014-06-09 15:52:52 +0700

    Change UX

--- apps/settings/js/hotkeydefination.js
+++ apps/settings/js/hotkeydefination.js
@@ -3,100 +3,124 @@
  */
 
 
-var downloadDialog = null;
+'use strict';
 
-var isShowDialog = false;
+var isChangeKey = false;
 
 const TIMEOUT = 2000;
 
+var ul;
+
+//Array save value key
+var valueKeyArr;
+
+//array id text key
+var valueActive =  ["key-home","key-mute","key-volume-up","key-volume-down"];
+
+//array radio-button key
+var valueCheck =  ["key-home-check","key-mute-check","key-volume-up-check","key-volume-down-check"];
+
+//position focus change key
+var position = -1;
+
 function keyEvent(event) {
 
-    if(isShowDialog)
-  {
+    if (isChangeKey) {
 
-    var charCode = event.keyCode || event.which;
-    var keychar = String.fromCharCode(charCode);
-
+        var charCode = event.keyCode || event.which;
+        var keychar = String.fromCharCode(charCode);
 
 
-        for(var i = 0 ; i <  HotKey['key'].length; i++){
+        for (var i = 0; i < HotKey['key'].length; i++) {
             if (charCode == 0) {
-                keychar = ""+event.key;
+                keychar = "" + event.key;
                 break;
             }
-            if(charCode == HotKey['key'][i].keyCode) {
+            if (charCode == HotKey['key'][i].keyCode) {
                 keychar = HotKey['key'][i].value;
                 break;
             }
         }
 
-     document.getElementById('key-chosse').innerHTML = "Changed Key : "+ keychar;
 
+        if (ul[0].className == "active") {
+            document.getElementById("key-home").innerHTML = keychar;
+            document.getElementById("key-home-check").checked = false;
+            valueKeyArr[0] = keychar;
+        }
+        else if (ul[1].className == "active") {
+            document.getElementById("key-mute").innerHTML = keychar;
+            document.getElementById("key-mute-check").checked = false;
+            valueKeyArr[1] = keychar
+        }
+        else if (ul[2].className == "active") {
+            document.getElementById("key-volume-up").innerHTML = keychar;
+            document.getElementById("key-volume-up-check").checked = false;
+            valueKeyArr[2] = keychar
+        }
+        else if (ul[3].className == "active") {
+            document.getElementById("key-volume-down").innerHTML = keychar;
+            document.getElementById("key-volume-down-check").checked = false;
+            valueKeyArr[3] = keychar
+        }
 
-
-    if(document.getElementById("key-home-check").checked){
-        document.getElementById("key-home").innerHTML = keychar;
-        document.getElementById("key-home-check").checked = false;
-    }else
-    if(document.getElementById("key-mute-check").checked){
-        document.getElementById("key-mute").innerHTML = keychar;
-	document.getElementById("key-mute-check").checked = false;
-    }else
-    if(document.getElementById("key-volume-down-check").checked){
-        document.getElementById("key-volume-down").innerHTML = keychar;
-	document.getElementById("key-volume-down-check").checked = false;
-    }else
-    if(document.getElementById("key-volume-up-check").checked){
-        document.getElementById("key-volume-up").innerHTML = keychar;
-	document.getElementById("key-volume-up-check").checked = false;
+        isChangeKey = false;
+        removeClassList();
     }
-
-
-   //wait to 2 seconds;
-    setTimeout(function() {
-        downloadDialog = document.getElementById('div-dialog');
-        downloadDialog.style.display = "none";
-        isShowDialog = false;
-    }, TIMEOUT);
-}
 }
 
-
-
-document.body.addEventListener("keydown",keyEvent,false);
+document.body.addEventListener("keydown", keyEvent, false);
 
 var clickHandler = {
     'key-home-check': function () {
-        downloadDialog = document.getElementById('div-dialog');
-        downloadDialog.style.display = "block";
-	    document.getElementById('key-chosse').innerHTML = "";
-        document.getElementById('text-dialog').innerHTML = "Type key from input keyboard : Home Key ";
-        isShowDialog = true;
+        clickKey(0);
     },
     'key-mute-check': function () {
-        downloadDialog = document.getElementById('div-dialog');
-        downloadDialog.style.display = "block";
-	document.getElementById('key-chosse').innerHTML = "";
-        document.getElementById('text-dialog').innerHTML = "Type key from input keyboard : Mute Key ";
-	isShowDialog = true;
+        clickKey(1);
     },
     'key-volume-up-check': function () {
-        downloadDialog = document.getElementById('div-dialog');
-        downloadDialog.style.display = "block";
-	document.getElementById('key-chosse').innerHTML = "";
-        document.getElementById('text-dialog').innerHTML = "Type key from input keyboard : Volume Up Key ";
-	isShowDialog = true;
+        clickKey(2);
     },
     'key-volume-down-check': function () {
-        downloadDialog = document.getElementById('div-dialog');
-        downloadDialog.style.display = "block";
-	document.getElementById('key-chosse').innerHTML = "";
-        document.getElementById('text-dialog').innerHTML = "Type key from input keyboard : Volume Down Key ";
-	isShowDialog = true;
+        clickKey(3);
+
     }
 }
 
-    document.body.addEventListener('click',function(evt){
-    if(clickHandler[evt.target.id])
-        clickHandler[evt.target.id || evt.target.dataset.fb].call(this,evt);
+function clickKey(value) {
+
+    if(!isChangeKey || ul[value].classList != 'active') {
+        removeClassList();
+        ul[value].classList.add("active");
+        position = value;
+        document.getElementById(valueActive[value]).innerHTML = "Input Hot key...";
+        changeKey();
+             }
+        else{
+        isChangeKey = false;
+        removeClassList();
+        }
+}
+
+function removeClassList() {
+    if (position != -1 && valueKeyArr.length != 0)
+        ul[position].getElementsByTagName('small')[0].innerHTML = valueKeyArr[position].toString();
+    valueKeyArr = new Array();
+    for (var i = 0; i < ul.length; i++) {
+        valueKeyArr.push(ul[i].getElementsByTagName('small')[0].innerHTML);
+        ul[i].classList.remove("active");
+        document.getElementById(valueCheck[i]).checked = false;
+    }
+}
+
+document.body.addEventListener('click', function (evt) {
+    ul = document.getElementById('hotkey-setup').getElementsByTagName('li');
+    if (clickHandler[evt.target.id])
+        clickHandler[evt.target.id || evt.target.dataset.fb].call(this, evt);
 });
+
+
+function changeKey() {
+    isChangeKey = true;
+}
+
