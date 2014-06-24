14ee15616dd3c3654ab8f5982d7adaa2baf25095
TienManhPtit <ninhmanh.tien@nomovok.vn>
2014-06-24 15:33:27 +0700

    Change UX Hot key Defination

--- apps/settings/js/hotkeydefination.js
+++ apps/settings/js/hotkeydefination.js
@@ -1,224 +1,238 @@
-/**
- * Created by TienNM on 5/16/14.
- */
-
 
 'use strict';
 
-var isChangeKey = false;
+// handle HotKey settings
+navigator.mozL10n.ready(function hotkeyDefination() {
 
-const TIMEOUT = 3000;
+    var _ = navigator.mozL10n.get;
 
-var ul;
+    var settings = window.navigator.mozSettings;
+    if (!settings)
+        return;
 
+    var isChangeKey = false;
+
+    const TIMEOUT = 3000;
+
+    var ul;
 //Array save value key
-var valueKeyArr;
-
-//array id text key
-var valueActive =  ["key-home","key-mute","key-volume-up","key-volume-down"];
-
-//array radio-button key
-var valueCheck =  ["key-home-check","key-mute-check","key-volume-up-check","key-volume-down-check"];
+    var valueKeyArr;
 
 //position focus change key
-var position = -1;
+    var position = -1;
 
-var gHotkeyManager = navigator.mozHotkeyManager;
-var homeKeyElement = document.getElementById("key-home");
-var homeKeyCheckElement = document.getElementById("key-home-check");
-var muteKeyElement = document.getElementById("key-mute");
-var muteKeyCheckElement = document.getElementById("key-mute-check");
-var volumeUpKeyElement = document.getElementById("key-volume-up");
-var volumeUpKeyCheckElement = document.getElementById("key-volume-up-check");
-var volumeDownKeyElement = document.getElementById("key-volume-down");
-var volumeDownKeyCheckElement = document.getElementById("key-volume-down-check");
-var errorElement = document.getElementById("hotkey-settings-error");
+    var gHotkeyManager = navigator.mozHotkeyManager;
 
-var keyElementArr = [homeKeyElement, muteKeyElement, volumeUpKeyElement, volumeDownKeyElement];
-var checkElementArr = [homeKeyCheckElement, muteKeyCheckElement, volumeUpKeyCheckElement, volumeDownKeyCheckElement];
+//    var homeKeyElement = document.getElementById("key-home");
+//    var homeKeyCheckElement = document.getElementById("key-home-check");
+//    var muteKeyElement = document.getElementById("key-mute");
+//    var muteKeyCheckElement = document.getElementById("key-mute-check");
+//    var volumeUpKeyElement = document.getElementById("key-volume-up");
+//    var volumeUpKeyCheckElement = document.getElementById("key-volume-up-check");
+//    var volumeDownKeyElement = document.getElementById("key-volume-down");
+//    var volumeDownKeyCheckElement = document.getElementById("key-volume-down-check");
 
-function init() {
-    function doInit(element, charCode) {
-        element.innerHTML = getKeyChar(charCode);
-    }
+    var errorElement = document.getElementById("hotkey-settings-error");
 
-    doInit(homeKeyElement, gHotkeyManager.homeKey);
-    doInit(muteKeyElement, gHotkeyManager.muteKey);
-    doInit(volumeUpKeyElement, gHotkeyManager.volumeUpKey);
-    doInit(volumeDownKeyElement, gHotkeyManager.volumeDownKey);
-}
 
-init();
+    var homeKeyCheckElement = document.querySelector('#key-home-check');
+    var homeKeyElement = document.querySelector('#key-home');
 
-function getKeyChar(charCode, event) {
-    var keyChar = "";
-    dump("_________ this is getKeyChar and charCode is " + charCode);
-    if (charCode == -1) {
-        keyChar = "No key";
-        return keyChar;
-    }
+    var muteKeyCheckElement = document.querySelector('#key-mute-check');
+    var muteKeyElement = document.querySelector('#key-mute');
 
-    if (charCode == 0) {
-        if (event) {
-            keyChar = "" + event.key;
-        } else {
+    var volumeUpKeyCheckElement = document.querySelector('#key-volume-up-check');
+    var volumeUpKeyElement = document.querySelector('#key-volume-up');
+
+    var volumeDownKeyCheckElement = document.querySelector('#key-volume-down-check');
+    var volumeDownKeyElement = document.querySelector('#key-volume-down');
+
+    var keyElementArr = [homeKeyElement, muteKeyElement, volumeUpKeyElement, volumeDownKeyElement];
+    var checkElementArr = [homeKeyCheckElement, muteKeyCheckElement, volumeUpKeyCheckElement, volumeDownKeyCheckElement];
+
+
+
+    homeKeyCheckElement.onclick = function k_homekeyPress() {
+        clickKey(0);
+    };
+    muteKeyCheckElement.onclick = function k_mutekeyPress() {
+        clickKey(1);
+    };
+    volumeUpKeyCheckElement.onclick = function k_volumeupkeyPress() {
+        clickKey(2);
+    };
+    volumeDownKeyCheckElement.onclick = function k_volumedownkeyPress() {
+        clickKey(3);
+    };
+
+
+    function getKeyChar(charCode, event) {
+        var keyChar = "";
+        dump("_________ this is getKeyChar and charCode is " + charCode);
+        if (charCode == -1) {
             keyChar = "No key";
-        }
-        return keyChar;
-    }
-
-    for (var i = 0; i < HotKey['key'].length; i++) {
-        if (charCode == HotKey['key'][i].keyCode) {
-            keyChar = HotKey['key'][i].value;
             return keyChar;
         }
+
+        if (charCode == 0) {
+            if (event) {
+                keyChar = "" + event.key;
+            } else {
+                keyChar = "No key";
+            }
+            return keyChar;
+        }
+
+        for (var i = 0; i < HotKey['key'].length; i++) {
+            if (charCode == HotKey['key'][i].keyCode) {
+                keyChar = HotKey['key'][i].value;
+                return keyChar;
+            }
+        }
+
+        return String.fromCharCode(charCode);
     }
 
-    return String.fromCharCode(charCode);
-}
+    function keyEvent(event) {
 
-function keyEvent(event) {
+        if (isChangeKey) {
 
-    if (isChangeKey) {
+            var charCode = event.keyCode || event.which;
+            var keychar = getKeyChar(charCode);
+            var displayString = "Changing key to: " + keychar;
+            var idx = -1;
 
-        var charCode = event.keyCode || event.which;
-        var keychar = getKeyChar(charCode);
-        var displayString = "Changing key to: " + keychar;
-        var idx = -1;
+            if (ul[0].className == "active") {
+                homeKeyElement.textContent = displayString;
+                homeKeyCheckElement.checked = false;
+                valueKeyArr[0] = keychar;
+                gHotkeyManager.setHomeKey(charCode);
+            }
+            else if (ul[1].className == "active") {
+                muteKeyElement.textContent = displayString;
+                muteKeyCheckElement.checked = false;
+                valueKeyArr[1] = keychar;
+                gHotkeyManager.setMuteKey(charCode);
+            }
+            else if (ul[2].className == "active") {
+                volumeUpKeyElement.textContent = displayString;
+                volumeUpKeyCheckElement.checked = false;
+                valueKeyArr[2] = keychar;
+                gHotkeyManager.setVolumeUpKey(charCode);
+            }
+            else if (ul[3].className == "active") {
+                volumeDownKeyElement.textContent = displayString;
+                volumeDownKeyCheckElement.checked = false;
+                valueKeyArr[3] = keychar;
+                gHotkeyManager.setVolumeDownKey(charCode);
+            }
 
-        if (ul[0].className == "active") {
-            homeKeyElement.innerHTML = displayString;
-            homeKeyCheckElement.checked = false;
-            valueKeyArr[0] = keychar;
-            gHotkeyManager.setHomeKey(charCode);
+            // isChangeKey = false;
+            // removeClassList();
+            // gHotkeyManager.endEditHotkey();
         }
-        else if (ul[1].className == "active") {
-            muteKeyElement.innerHTML = displayString;
-            muteKeyCheckElement.checked = false;
-            valueKeyArr[1] = keychar;
-            gHotkeyManager.setMuteKey(charCode);
-        }
-        else if (ul[2].className == "active") {
-            volumeUpKeyElement.innerHTML = displayString;
-            volumeUpKeyCheckElement.checked = false;
-            valueKeyArr[2] = keychar;
-            gHotkeyManager.setVolumeUpKey(charCode);
-        }
-        else if (ul[3].className == "active") {
-            volumeDownKeyElement.innerHTML = displayString;
-            volumeDownKeyCheckElement.checked = false;
-            valueKeyArr[3] = keychar;
-            gHotkeyManager.setVolumeDownKey(charCode);
-        }
-
-        // isChangeKey = false;
-        // removeClassList();
-        // gHotkeyManager.endEditHotkey();
     }
-}
 
-document.body.addEventListener("keydown", keyEvent, false);
 
-var clickHandler = {
-    'key-home-check': function () {
-        clickKey(0);
-    },
-    'key-mute-check': function () {
-        clickKey(1);
-    },
-    'key-volume-up-check': function () {
-        clickKey(2);
-    },
-    'key-volume-down-check': function () {
-        clickKey(3);
+    function init() {
+        function doInit(element, charCode) {
+            element.textContent = getKeyChar(charCode);
+        }
 
+        doInit(homeKeyElement, gHotkeyManager.homeKey);
+        doInit(muteKeyElement, gHotkeyManager.muteKey);
+        doInit(volumeUpKeyElement, gHotkeyManager.volumeUpKey);
+        doInit(volumeDownKeyElement, gHotkeyManager.volumeDownKey);
+
+        ul = document.getElementById('hotkey-setup').getElementsByTagName('li');
+
+        console.log("Init");
     }
-}
 
-function clickKey(value) {
+    init();
 
-    if(!isChangeKey || ul[value].classList != 'active') {
-        dump("-----------------We'll start editing hotkey! at " + value);
-        gHotkeyManager.beginEditHotkey();
-        removeClassList();
-        ul[value].classList.add("active");
-        position = value;
-        // document.getElementById(valueActive[value]).innerHTML = "Press a key...";
-        keyElementArr[value].innerHTML = "Press a key";
-        changeKey();
-    } else {
+
+    function clickKey(value) {
+
+        if(!isChangeKey || ul[value].classList != 'active') {
+            dump("-----------------We'll start editing hotkey! at " + value);
+            gHotkeyManager.beginEditHotkey();
+            removeClassList();
+            ul[value].classList.add("active");
+            position = value;
+            // document.getElementById(valueActive[value]).innerHTML = "Press a key...";
+            keyElementArr[value].textContent = "Press a key";
+            changeKey();
+        } else {
+            isChangeKey = false;
+            removeClassList();
+            gHotkeyManager.endEditHotkey();
+        }
+    }
+
+    function removeClassList() {
+        if (position != -1 && valueKeyArr.length != 0) {
+            // ul[position].getElementsByTagName('small')[0].innerHTML = valueKeyArr[position].toString();
+            keyElementArr[position].textContent = valueKeyArr[position].toString();
+        }
+        valueKeyArr = new Array();
+        var numKeySettings = ul.length; // the last element is the error message
+        for (var i = 0; i < numKeySettings; i++) {
+            // valueKeyArr.push(ul[i].getElementsByTagName('small')[0].innerHTML);
+            valueKeyArr.push(keyElementArr[i].textContent);
+            ul[i].classList.remove("active");
+            checkElementArr[i].checked = false;
+        }
+    }
+
+    function changeKey() {
+        isChangeKey = true;
+    }
+
+    function showError() {
+        errorElement.style.display = "block";
+        setTimeout(hideError, TIMEOUT);
+    }
+
+    function hideError() {
+        errorElement.style.display = "none";
+    }
+
+    function onHotkeySetResult(event) {
+        dump("This is the handler for hotkeySetResult: " + event["type"]);
+
+        var type = event["type"];
+        var data = type.split(":");
+        if (data[1] == "0") {
+            dump("Set hotkey ok!");
+        } else {
+            dump("Set hotkey error!");
+            // need to show some error!
+            switch (position) {
+                case 0:
+                    valueKeyArr[0] = getKeyChar(gHotkeyManager.homeKey);
+                    break;
+                case 1:
+                    valueKeyArr[1] = getKeyChar(gHotkeyManager.muteKey);
+                    break;
+                case 2:
+                    valueKeyArr[2] = getKeyChar(gHotkeyManager.volumeUpKey);
+                    break;
+                case 3:
+                    valueKeyArr[3] = getKeyChar(gHotkeyManager.volumeDownKey);
+                    break;
+            }
+
+            showError();
+        }
         isChangeKey = false;
         removeClassList();
         gHotkeyManager.endEditHotkey();
     }
-}
 
-function removeClassList() {
-    if (position != -1 && valueKeyArr.length != 0) {
-        // ul[position].getElementsByTagName('small')[0].innerHTML = valueKeyArr[position].toString();
-        keyElementArr[position].innerHTML = valueKeyArr[position].toString();
-    }
-    valueKeyArr = new Array();
-    var numKeySettings = ul.length; // the last element is the error message
-    for (var i = 0; i < numKeySettings; i++) {
-        // valueKeyArr.push(ul[i].getElementsByTagName('small')[0].innerHTML);
-        valueKeyArr.push(keyElementArr[i].innerHTML);
-        ul[i].classList.remove("active");
-        checkElementArr[i].checked = false;
-    }
-}
+    gHotkeyManager.onHotkeySetResult = onHotkeySetResult;
 
-document.body.addEventListener('click', function (evt) {
-    ul = document.getElementById('hotkey-setup').getElementsByTagName('li');
-    if (clickHandler[evt.target.id])
-        clickHandler[evt.target.id || evt.target.dataset.fb].call(this, evt);
+
+    document.body.addEventListener("keydown", keyEvent, false);
+
 });
-
-
-function changeKey() {
-    isChangeKey = true;
-}
-
-function showError() {
-    errorElement.style.display = "block";
-    setTimeout(hideError, TIMEOUT);
-}
-
-function hideError() {
-    errorElement.style.display = "none";
-}
-
-function onHotkeySetResult(event) {
-    dump("This is the handler for hotkeySetResult: " + event["type"]);
-
-    var type = event["type"];
-    var data = type.split(":");
-    if (data[1] == "0") {
-        dump("Set hotkey ok!");
-    } else {
-        dump("Set hotkey error!");
-        // need to show some error!
-        switch (position) {
-        case 0:
-            valueKeyArr[0] = getKeyChar(gHotkeyManager.homeKey);
-            break;
-        case 1:
-            valueKeyArr[1] = getKeyChar(gHotkeyManager.muteKey);
-            break;
-        case 2:
-            valueKeyArr[2] = getKeyChar(gHotkeyManager.volumeUpKey);
-            break;
-        case 3:
-            valueKeyArr[3] = getKeyChar(gHotkeyManager.volumeDownKey);
-            break;
-        }
-
-        showError();
-    }
-    isChangeKey = false;
-    removeClassList();
-    gHotkeyManager.endEditHotkey();
-}
-
-gHotkeyManager.onHotkeySetResult = onHotkeySetResult;
-
