d149a97840a3118145d7eeed259ecbd8aebabbd2
DeanHuangTW <DeanHuang@via.com.tw>
2014-08-08 14:02:43 +0800

    Support Ethernet Static IP
    Copy rock2's ethernet source code to here

--- apps/settings/js/ethernet.js
+++ apps/settings/js/ethernet.js
@@ -4,232 +4,290 @@
 navigator.mozL10n.ready(function ethernetSettings() {
   var _ = navigator.mozL10n.get;
   
-  var settings = window.navigator.mozSettings;
-  if (!settings)
-    return;
-    
+   
   var gEthernetManager = navigator.mozEthernetManager;
+  if (!gEthernetManager) {
+    return;
+  }
+  
+  const Field = {
+    IP: 1,
+    Gateway: 2,
+    Netmask: 3,
+    DNS1: 4,
+    DNS2: 5
+  };
 
-  var gEthernet = document.querySelector('#ethernet');
-  var gEthernetCheckBox = document.querySelector('#ethernet-enabled input');
-  var gEthernetInfoBlock = document.querySelector('#ethernet-desc');
-  var gEthernetDynamic = document.querySelector('#ethernet-dynamic');
-  var gEthernetDynamicCheckBox = document.querySelector('#ethernet-dynamic input');
-  var gEthernetManual = document.querySelector('#ethernet-manual');
-  var gEthernetManualCheckBox = document.querySelector('#ethernet-manual input');
-  
-  var gEthernetIpAddress = document.querySelector('#ethernet-ip-address');
-  var gEthernetSmallLabelIpAddress = document.querySelector('#ethernet-ip-address-small-label');
-  
-  var gEthernetGateway = document.querySelector('#ethernet-gateway');
-  var gEthernetSmallLabelGateway = document.querySelector('#ethernet-gateway-small-label');
-  
-  var gEthernetNetmark = document.querySelector('#ethernet-netmark');
-  var gEthernetSmallLabelNetmark = document.querySelector('#ethernet-netmark-small-label');
-  
-  var gEthernetDNS1 = document.querySelector('#ethernet-dns1');
-  var gEthernetSmallLabelDNS1 = document.querySelector('#ethernet-dns1-small-label');
-  
-  var gEthernetDNS2 = document.querySelector('#ethernet-dns2');
-  var gEthernetSmallLabelDNS2 = document.querySelector('#ethernet-dns2-small-label');
-  
-  var gIsEthernetDynamic = true;
-  var gEthernetIpAddressValue = "192.168.0.13";
-  var gEthernetGatewayValue = "22.11.2.2";
-  var gEthernetNetmarkValue = "33.34.3";
-  var gEthernetDNS1Value = "545";
-  var gEthernetDNS2Value = "3543";
-  
-  //Currently, only DHCP is supported
-  gEthernetDynamicCheckBox.disabled = false;
-  gEthernetManualCheckBox.disabled = true;
-  
-  //manual settings elements
-  var gUpdateManualDialog = document.getElementById('update-manual-dialog');
-  var gUpdateManualInput = document.getElementById('update-manual-input');
-  var gUpdateManualCancelButton = document.getElementById('update-manual-cancel');
-  var gUpdateManualConfirmButton = document.getElementById('update-manual-confirm');
-  var gUpdateManualTitle = document.getElementById('update-manual-dialog-title');
-  
-  function updateVisibilityStatus() {
-    if (gEthernetManager.enabled) {
-      gEthernetDynamic.hidden = false;
-      gEthernetManual.hidden = false;
-      
-      gEthernetIpAddress.hidden = false;
-      gEthernetGateway.hidden = false;
-      gEthernetNetmark.hidden = false;
-      gEthernetDNS1.hidden = false;
-      gEthernetDNS2.hidden = false;
-    } else {
-      gEthernetDynamic.hidden = true;
-      gEthernetManual.hidden = true;
-      gEthernetIpAddress.hidden = true;
-      gEthernetGateway.hidden = true;
-      gEthernetNetmark.hidden = true;
-      gEthernetDNS1.hidden = true;
-      gEthernetDNS2.hidden = true;
+  function getL10NText(l10n_id, defaultText) {
+    var text = _(l10n_id);
+    return text ? text : defaultText;
+  };
+
+  const TextValue = {
+    IP: getL10NText("ethernet-ip-address", "IP address"),
+    Gateway: getL10NText("ethernet-gateway", "Gateway"),
+    Netmask: getL10NText("ethernet-netmask", "Netmask"),
+    DNS1: getL10NText("ethernet-dns1", "DNS1"),
+    DNS2: getL10NText("ethernet-dns2", "DNS2"),
+    DescDhcp: getL10NText("ethernet-dhcp-desc", "Automatic configure network address"),
+    DescManual: getL10NText("ethernet-manual-desc", "Manual configure network address"),
+  };
+
+  // main UX bussiness
+  var MainUX = {
+    ethernetToggle: document.querySelector('#ethernet-enabled input'),
+    dhcpToggleArea: document.querySelector('#ethernet-dhcp'),
+    dhcpToggle: document.querySelector('#ethernet-dhcp input'),
+    dhcpToggleDesc: document.querySelector('#ethernet-dhcp-small-label'),
+    detailsConfig: document.querySelector("#ethernet-settings-details"),
+    ipInfo: document.querySelector('#ethernet-ip-address-small-label'),
+    gatewayInfo: document.querySelector('#ethernet-gateway-small-label'),
+    netmaskInfo: document.querySelector('#ethernet-netmask-small-label'),
+    dns1Info: document.querySelector('#ethernet-dns1-small-label'),
+    dns2Info: document.querySelector('#ethernet-dns2-small-label'),
+
+    init: function() {
+      this.update();
+      this.connectEvents();
+    },
+
+    onEthernetEnabledChanged: function(enabled) {
+      this.ethernetToggle.checked = enabled;
+      this.dhcpToggleArea.hidden = !enabled;
+      this.detailsConfig.hidden = !enabled;
+    },
+
+    onDhcpChanged: function(enabled) {
+      if (this.dhcpToggle.checked != enabled) {
+        this.dhcpToggle.checked = enabled;
+      }
+      if (enabled) {
+        this.detailsConfig.classList.add("disabled");
+        this.dhcpToggleDesc.textContent = TextValue.DescDhcp;
+        this.updateDetailsInfo(gEthernetManager.connection);
+      } else {
+        this.detailsConfig.classList.remove("disabled");
+        this.dhcpToggleDesc.textContent = TextValue.DescManual;
+        this.updateDetailsInfo(gEthernetManager.staticconfig);
+      }
+    },
+
+    updateDetailsInfo: function(info) {
+      this.ipInfo.textContent = info.ip;
+      this.gatewayInfo.textContent = info.gateway;
+      this.netmaskInfo.textContent = info.netmask;
+      this.dns1Info.textContent = info.dns1;
+      this.dns2Info.textContent = info.dns2;
+    },
+
+    update: function() {
+      MainUX.onEthernetEnabledChanged(gEthernetManager.enabled);
+      if (!gEthernetManager.enabled) {
+        // nothing more to update
+        return;
+      }
+
+      MainUX.onDhcpChanged(gEthernetManager.dhcp);
+    },
+
+    setIP: function(val) {
+      this.ipInfo.textContent = val;
+    },
+
+    setGateway: function(val) {
+      this.gatewayInfo.textContent = val;
+    },
+
+    setNetmask: function(val) {
+      this.netmaskInfo.textContent = val;
+    },
+
+    setDNS1: function(val) {
+      this.dns1Info.textContent = val;
+    },
+
+    setDNS2: function(val) {
+      this.dns2Info.textContent = val;
+    },
+
+    connectEvents: function() {
+      this.ethernetToggle.onchange = function() {
+        if (this.ethernetToggle.checked) {
+          Connectivity.enableEthernet();
+        } else {
+          Connectivity.disableEthernet();
+        }
+      }.bind(this);
+
+      this.dhcpToggle.onchange = function() {
+        gEthernetManager.setdhcp(this.dhcpToggle.checked);
+      }.bind(this);
+
+      function connectEditor(elementSelector, field) {
+        var element = document.querySelector(elementSelector);
+        if (element) {
+          element.onclick = function() {
+            IPAddressEditor.showEditor(field);
+          }
+        } else {
+          dump("invalid element   " + elementSelector);
+        }
+      };
+
+      connectEditor('#ethernet-ip-address', Field.IP);
+      connectEditor('#ethernet-gateway', Field.Gateway);
+      connectEditor('#ethernet-netmask', Field.Netmask);
+      connectEditor('#ethernet-dns1', Field.DNS1);
+      connectEditor('#ethernet-dns2', Field.DNS2);
     }
   };
-  
-  function showConnectionInfo() {
-    if (gEthernetCheckBox.checked != gEthernetManager.enabled) {
-      gEthernetCheckBox.checked = gEthernetManager.enabled;
-    }
-    if (gEthernetDynamicCheckBox.checked != gIsEthernetDynamic) {
-      gEthernetDynamicCheckBox.checked = gIsEthernetDynamic;
-    }
-    
-    if (gEthernetManualCheckBox.checked == gIsEthernetDynamic) {
-      gEthernetManualCheckBox.checked = !gIsEthernetDynamic;
-    }
-    
-    if (gEthernetManager.enabled) {
-      gEthernetSmallLabelIpAddress.textContent = gEthernetIpAddressValue;
-      gEthernetSmallLabelGateway.textContent = gEthernetGatewayValue;
-      gEthernetSmallLabelNetmark.textContent = gEthernetNetmarkValue;
-      gEthernetSmallLabelDNS1.textContent = gEthernetDNS1Value;
-      gEthernetSmallLabelDNS2.textContent = gEthernetDNS2Value;
-    }
+
+  // this is the ux handling for settings elemenents inside Left panel of Settings App!
+  // some how this should be put in Connectivity.js instead!
+  var SettingsElement = {
+    ethernetElement: document.querySelector('#ethernet-desc'),
+
+    onEthernetEnabledChanged: function(enabled) {
+      var text = enabled ? _('enabled') : _('disabled');
+      this.ethernetElement.textContent = text;
+    },
+  };
+
+  var EventProcessor = {
+    init: function() {
+      this.initConnectivityCallbacks();
+      this.initEthernetManagerCallbacks();
+      this.initSettingsAppCallbacks();
+    },
+
+    initConnectivityCallbacks: function() {
+      Connectivity.ethernetEnabledChanged = function() {
+        SettingsElement.onEthernetEnabledChanged(gEthernetManager.enabled);
+        MainUX.onEthernetEnabledChanged(gEthernetManager.enabled);
+      };
+
+      Connectivity.ethernetConnectedChanged = function() {
+        // nothing to do here, actually. This is useful for the notification icon only
+      };
+    },
+
+    initEthernetManagerCallbacks: function() {
+      gEthernetManager.ondhcpchanged = function() {
+        MainUX.onDhcpChanged(gEthernetManager.dhcp);
+      };
+
+      gEthernetManager.onconnectionupdated = function() {
+        if (gEthernetManager.enabled && gEthernetManager.dhcp) {
+          MainUX.updateDetailsInfo(gEthernetManager.connection);
+        }
+      }
+    },
+
+    initSettingsAppCallbacks: function() {
+      this.ethernetElement = document.querySelector('#ethernet');
+      this.ethernetElement.addEventListener('transitionend', function(evt) {
+        if (evt.target == this.ethernetElement) {
+          MainUX.update();
+        }
+      });
+
+      document.addEventListener('visibilitychange', MainUX.update);
+    },
   };
   
-  function updateConnectionInfo() {
-    console.log("updateConnectionInfo");
-    gEthernetIpAddressValue = gEthernetManager.ipAddress;
-    gEthernetGatewayValue = gEthernetManager.gateway;
-    gEthernetNetmarkValue = gEthernetManager.netmask;
-    gEthernetDNS1Value = gEthernetManager.dns1;
-    gEthernetDNS2Value = gEthernetManager.dns2;
+  var IPAddressEditor = {
+    editorWidget: document.getElementById('update-manual-dialog'),
+    titleLabel: document.getElementById('update-manual-dialog-title'),
+    inputEntry: document.getElementById('update-manual-input'),
+    _init: false,
+    _field: 0,
+
+    init: function() {
+      if (!this._init) {
+        this.editorWidget.hidden = true;
+        this.connectEvents();
+        this._init = true;
+      }
+    },
+
+    showEditor: function(field) {
+      this.init();
+      this._field = field;
+      switch (field) {
+        case Field.IP:
+          this._setTitleValue(TextValue.IP, gEthernetManager.staticconfig.ip);
+          break;
+        case Field.Gateway:
+          this._setTitleValue(TextValue.Gateway, gEthernetManager.staticconfig.gateway);
+          break;
+        case Field.Netmask:
+          this._setTitleValue(TextValue.Netmask, gEthernetManager.staticconfig.netmask);
+          break;
+        case Field.DNS1:
+          this._setTitleValue(TextValue.DNS1, gEthernetManager.staticconfig.dns1);
+          break;
+        case Field.DNS2:
+          this._setTitleValue(TextValue.DNS2, gEthernetManager.staticconfig.dns2);
+          break;
+      }
+
+      this.editorWidget.hidden = false;
+
+      // Focus the input field to trigger showing the keyboard
+      this.inputEntry.focus();
+      var cursorPos = this.inputEntry.value.length;
+      this.inputEntry.setSelectionRange(0, cursorPos);
+    },
+
+    onCancel: function() {
+      this._field = 0;
+      this.editorWidget.hidden = true;
+    },
+
+    onOk: function() {
+      var value = this.inputEntry.value;
+      switch (this._field) {
+        case Field.IP:
+          gEthernetManager.setaddr(value);
+          MainUX.setIP(value);
+          break;
+        case Field.Gateway:
+          gEthernetManager.setgateway(value);
+          MainUX.setGateway(value);
+          break;
+        case Field.Netmask:
+          gEthernetManager.setnetmask(value);
+          MainUX.setNetmask(value);
+          break;
+        case Field.DNS1:
+          gEthernetManager.setdns1(value);
+          MainUX.setDNS1(value);
+          break;
+        case Field.DNS2:
+          gEthernetManager.setdns2(value);
+          MainUX.setDNS2(value);
+          break;
+        default:
+          console.error("Error: Invalid field!" );
+      }
+
+      this._field = 0;
+      this.editorWidget.hidden = true;
+    },
+
+    _setTitleValue: function(title, value) {
+      this.titleLabel.textContent = title;
+      this.inputEntry.value = value;
+    },
+
+    connectEvents: function() {
+      var cancelButton = document.getElementById('update-manual-cancel');
+      cancelButton.onclick = this.onCancel.bind(this);
+      var okButton = document.getElementById('update-manual-confirm');
+      okButton.onclick = this.onOk.bind(this);
+    }
   }
-  
-  function showUpdateManualDialog(aDialogTitle, aCurrentValue) {
-    gUpdateManualTitle.textContent = aDialogTitle;
-    gUpdateManualInput.value = aCurrentValue;
-    gUpdateManualDialog.hidden = false;
-    
-    // Focus the input field to trigger showing the keyboard
-    gUpdateManualInput.focus();
-    var cursorPos = gUpdateManualInput.value.length;
-    gUpdateManualInput.setSelectionRange(0, cursorPos);
-  };
-  
-  var self = this;
-  document.addEventListener('visibilitychange', updateVisibilityStatus);
-  document.addEventListener('visibilitychange', showConnectionInfo);
-  gEthernet.addEventListener('transitionend', function(evt) {
-    if (evt.target == gEthernet) {
-      updateVisibilityStatus();
-      showConnectionInfo();
-    }
-  });
-  
-  gEthernetCheckBox.onchange = function e_toggleEthernet() {
-    if (gEthernetCheckBox.checked) {
-      Connectivity.enableEthernet();
-    } else {
-      Connectivity.disableEthernet();
-    }
-    gEthernetCheckBox.disabled = true;
-  };
-  
-  Connectivity.ethernetEnabledChanged = function() {
-    updateVisibilityStatus();
-    if (gEthernetManager.enabled) {
-      gEthernetInfoBlock.textContent = _('enabled');
-      updateVisibilityStatus();
-      showConnectionInfo();
-    } else {
-      gEthernetInfoBlock.textContent = _('disabled');
-    }
-    gEthernetCheckBox.disabled = false;
-  };
-  
-  Connectivity.ethernetConnectedChanged = function() {
-    if (gEthernetManager.connected) {
-      gEthernetCheckBox.disabled = false;
-      updateVisibilityStatus();
-      showConnectionInfo();
-    } else {
-    }
-  };
-  
-  gEthernetDynamicCheckBox.onchange = function e_toggleDynamic() {
-    gIsEthernetDynamic = this.checked;
-    gEthernetManualCheckBox.checked = !gIsEthernetDynamic;
-    updateVisibilityStatus();
-    if (gIsEthernetDynamic) {
-      showConnectionInfo();
-    }
-  };
-  
-  gEthernetManualCheckBox.onchange = function e_toggleManual() {
-    gIsEthernetDynamic = !this.checked;
-    gEthernetDynamicCheckBox.checked = gIsEthernetDynamic;
-    updateVisibilityStatus();
-    if (gIsEthernetDynamic) {
-      showConnectionInfo();
-    }
-  };
-  
-  gEthernetIpAddress.onclick = function e_updateIpAddress() {
-    if (!gIsEthernetDynamic) {
-      showUpdateManualDialog("IP-Address", gEthernetIpAddressValue);
-    }
-  };
-  gEthernetGateway.onclick = function e_updateGateway() {
-    if (!gIsEthernetDynamic) {
-      showUpdateManualDialog("Gateway", gEthernetGatewayValue);
-    }
-  };
-  gEthernetNetmark.onclick = function e_updateNetmark() {
-    if (!gIsEthernetDynamic) {
-      showUpdateManualDialog("Netmark", gEthernetNetmarkValue);
-    }
-  };
-  gEthernetDNS1.onclick = function e_updateDNS1() {
-    if (!gIsEthernetDynamic) {
-      showUpdateManualDialog("DNS1", gEthernetDNS1Value);
-    }
-  };
-  gEthernetDNS2.onclick = function e_updateDNS2() {
-    if (!gIsEthernetDynamic) {
-      showUpdateManualDialog("DNS2", gEthernetDNS2Value);
-    }
-  };
-  gUpdateManualCancelButton.onclick = function e_updateCanceled() {
-    gUpdateManualDialog.hidden = true;
-  };
-  gUpdateManualConfirmButton.onclick = function e_updateConfirmed() {
-    gUpdateManualDialog.hidden = true;
-    
-    switch (gUpdateManualTitle.textContent) {
-      case 'IP-Address':
-        gEthernetIpAddressValue = gUpdateManualInput.value;
-        break;
-      case 'Gateway':
-        gEthernetGatewayValue = gUpdateManualInput.value;
-        break;
-      case 'Netmark':
-        gEthernetNetmarkValue = gUpdateManualInput.value;
-        break;
-      case 'DNS1':
-        gEthernetDNS1Value = gUpdateManualInput.value;
-        break;
-      case 'DNS2':
-        gEthernetDNS2Value = gUpdateManualInput.value;
-        break;
-      default:
-    }
-    showConnectionInfo();
-  };
-  
-  //Update at start-up
-  updateVisibilityStatus();
-  updateConnectionInfo();
-  showConnectionInfo();
-  if (gEthernetManager.enabled) {
-    gEthernetInfoBlock.textContent = _('enabled');
-  } else {
-    gEthernetInfoBlock.textContent = _('disabled');
-  }
+
+  MainUX.init();
+  EventProcessor.init();
 });
